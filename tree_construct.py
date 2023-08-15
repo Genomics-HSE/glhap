0c14812969a7d6f00dc9826f3d3001a37499e99d
Nikita Gayanov <nikitaminecraft@gmail.com>
2023-08-15 15:07:04 +0300

    divide

--- tree_construct.py
+++ tree_construct.py
@@ -22,5 +22,81 @@ class Node(NodeMixin):
         return self.name
     
     
+def get_snp(node):
+    '''
+    Returns snps fron node: Node
+
+    Paramters:
+    node : Node
+    node in phylogenetic tree
+
+    Returns:
+    list of lists of [old_nucleotide,new_nucleotid, position]
+    '''
+    snp = list()
+    atgc = set(['A','T','G','C','a','t','g','c'])
+    for i in range(2,len(node)):
+        if node[i][0] in atgc and node[i][-1] in atgc:
+            snp.append([node[i][0],node[i][-1],int(node[i][1:-1])])
+    return snp
+
+
+def get_insertion(node):
+    '''
+    Currently unused
+    '''
+    ins = set()
+    for i in range(2,len(node)):
+        if '.' in node[i]:
+            dot_pos = node[i].find('.')
+            pos = int(node[i][1:dot_pos])
+#             size = ''.join(k for k in node[i][dot_pos+1:] if  k.isdigit())
+#             insert = ''.join(k for k in node[i][dot_pos+1:] if  k.isalpha())
+            ins.add(pos)
+    return ins
+
+
+def get_deletion(node):
+    '''
+    Currently unused
+    '''
+    deletion = set()
+    for i in range(2,len(node)):
+        if node[i][-1]=='d':
+            if node[i][0].isalpha():
+                deletion.add(int(node[i][1:-1]))
+            else:
+                dash_pos = node[i].find('-')
+                deletion.add(int(node[i][1:dash_pos]))
+    return deletion
+
+
+def make_tree(tree,node,pos=0):    
+    '''
+    Initializate tree structure of phylogenetic tree
+    Parameters:
+    tree: list
+    list from phylotree-parse
+    node: Node
+    root node
+    pos: int
+    unnecessary parameter. position of current node in tree list
+    
+    '''
 
     
+    posit = pos + 1
+    i = posit
+    while i<len(tree) and tree[i][0] >= tree[pos][0]+1:
+        if tree[i][0] == tree[pos][0]+1:
+            # print(node.name,tree[i][1])
+            snps = get_snp(tree[i])
+            # insertion = get_insertion(tree[i])
+            insertion = set()
+            # deletion = get_deletion(tree[i])
+            deletion = set()
+            tmp = Node(tree[i][1],snps,insertion,parent=node)
+            
+            make_tree(tree,tmp,i)
+
+        i += 1        
